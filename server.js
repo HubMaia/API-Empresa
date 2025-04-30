71ce0c7910735341e1932f7082bfaa9ce27d241f
HubMaia <tecmaia7@gmail.com>
2025-04-29 22:28:46 -0300

    Adicionando index.html

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const swaggerUi = require('swagger-ui-express');
 const swaggerJsdoc = require('swagger-jsdoc');
 const routes = require('./routes/funcionarios');
 const swaggerDocs = require('./swagger/swaggerDocs');
+const path = require('path');
 
 // Configuração do banco de dados
 const db = require('./config/db');
@@ -15,12 +16,20 @@ const port = 3000;
 app.use(cors());
 app.use(express.json());
 
+// Servir arquivos estáticos da pasta public
+app.use(express.static(path.join(__dirname, 'public')));
+
 // Configuração do Swagger
 const specs = swaggerJsdoc(swaggerDocs);
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(specs));
 app.get('/swagger.json', (req, res) => {
     res.setHeader('Content-Type', 'application/json');
     res.send(specs);
+});
+
+// Rota raiz
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
 // Usar as rotas
